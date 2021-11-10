e873fdb3a7b344b1e77167176193a67b602b62a9
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-11-10 13:59:46 -0600

    added a model and DbContext

---
+++ E-Commerce-App/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using E_Commerce_App.Models;
+
+namespace E_Commerce_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Home!");
+            return View();
+        }
+
+        [HttpGet("PrivacyPolicy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet("Test/404")]
+        public IActionResult TestNotFound()
+        {
+            return NotFound();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ E-Commerce-App/Data/ECommerceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using E_Commerce_App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_App.Data
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
