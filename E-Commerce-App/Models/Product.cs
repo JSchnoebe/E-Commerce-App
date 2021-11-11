b351c648395bffe9f6b3b54d6d17862cf7cd47b0
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-11-11 09:57:13 -0600

    added display name for product

--- E-Commerce-App/Models/Product.cs
+++ E-Commerce-App/Models/Product.cs
@@ -7,13 +7,16 @@ namespace E_Commerce_App.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Product Name")]
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
 
+        [Display(Name = "Price")]
         [Required]
         public decimal Price { get; set; }
 
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
         public Category Category { get; set; }
