174a4a2bb1eaeb1e8dcba69d14c9db18b8ef8077
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-11-22 13:30:33 -0600

    added the cart page

---
+++ E-Commerce-App/Pages/CartPage.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_Commerce_App.Pages
+{
+    public class CartPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- E-Commerce-App/Services/IProductRepository.cs
+++ E-Commerce-App/Services/IProductRepository.cs
@@ -24,12 +24,6 @@ namespace E_Commerce_App.Controllers
 
         public async Task<List<Product>> GetAll()
         {
-            //return new List<Category>
-            //{
-            //    new Category { Id = 1, Name = "Games" },
-            //    new Category { Id = 2, Name = "Books" },
-            //    new Category { Id = 3, Name = "Toys" },
-            //};
             return await _context.Products.ToListAsync();
         }
 
